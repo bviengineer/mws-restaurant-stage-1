81a1724c58eebe23dc6963a10a9a418a2a2bafaa
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-04 14:19:09 -0400

    Updated MapBox API key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYnZpZ2lybGRldiIsImEiOiJjamxsZGJ6ZDUwd2N4M2tteHZ4azRud3N5In0.HaywLez2ZucWzECLwTuNLw>',
+        mapboxToken: 'pk.eyJ1IjoiYnZpZ2lybGRldiIsImEiOiJjamxsZGJ6ZDUwd2N4M2tteHZ4azRud3N5In0.HaywLez2ZucWzECLwTuNLw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
