29b34fe58bebf131cf9cf80ba882d2d1e67f5721
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-10 21:24:13 -0400

    Add tabindex to resturant list & add accessbilibty roles to defined elements

--- js/main.js
+++ js/main.js
@@ -178,8 +178,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = "5";
   li.append(more)
-
   return li
 }
 
