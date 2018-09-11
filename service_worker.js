5b61f9c795c81b48814b0de22a176299b953a094
BVIDev <16726593+bviengineer@users.noreply.github.com>
2018-09-11 08:54:32 -0400

    Rename service worker file and add files to be used offline in an array

---
+++ service_worker.js
@@ -0,0 +1,15 @@
+console.log("Service Worker Successfully Registered");
+
+self.addEventListener("fetch", function(event){
+    console.log(event.request);
+});
+
+const offlineFiles = [
+    "/",
+    "index.html", "restaurant.html",
+    "css/styles.css",
+    "/data/restaurants.json",
+    "/js/main.js", "/js/restaurant_info.js", "/js/dbhelper.js",
+    "/img/1.jpg", "/img/2.jpg", "/img/3.jpg", "/img/4.jpg", "/img/5.jpg",
+    "/img/6.jpg", "/img/7.jpg", "/img/8.jpg", "/img/9.jpg", "/img/10.jpg"
+];
